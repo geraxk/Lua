f95fe20ce58f2da203a18b41bc2e60c295f6cc69
6erax <101382801+geraxk@users.noreply.github.com>
2022-08-21 20:08:51 -0500

    Random String Generator [Lua]
    This is a module for generate easily random strings combinated with numbers.
    How to use it:
    Only require it for example
    print(require(script.Parent.ModuleScript):Generate())
    and you gonna get on your output a random string!

---
+++ RandomGenerator.lua
@@ -0,0 +1,23 @@
+-- Module created by @6erax
+-- This module work for creating random strings like passwords etc
+local Upper = {
+	"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
+	"V","W","X","Y","Z"
+}
+local function Case()
+	local Numberorupper = math.random(1,3)
+	if Numberorupper == 1  then
+		return Upper[math.random(1,#Upper)]
+	end
+	if Numberorupper == 2 then 
+		return string.lower(Upper[math.random(1,#Upper)])
+	end
+	if Numberorupper == 3 then
+		return math.floor(math.random(1,10))
+	end
+end
+local Module = {}
+function Module:Generate()
+	return Case()..Case()..Case()..Case()..Case()..Case()..Case()..Case()..Case()..Case()..Case()..Case()
+end
+return Module
